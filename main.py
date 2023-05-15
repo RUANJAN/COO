8279594aa9ef35eaea9d85a726e18699971c3af0
RUANJAN <128681138+RUANJAN@users.noreply.github.com>
2023-05-15 14:28:03 +0800

    m.py
    新改的有板折線圖

--- main.py
+++ main.py
@@ -1,106 +1,115 @@
 import tkinter as tk
 from tkinter import ttk
-from tkinter import Button, Frame
-from datasoerce import CarbonFootPrint
 from PIL import Image, ImageTk
-import csv
-# 導入資料庫模組
-from datasoerce import *
+import matplotlib.pyplot as plt
+import datasource
 
-
-class Window(tk.Tk):
+class MyWindow(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        # ----------------建立畫布--------------------
-        ttkStyle = ttk.Style()
-        ttkStyle.theme_use('default')  # 畫布
-        ttkStyle.configure('TLabelframe', borderwidth=0)
+        self.selectdata = None  # Placeholder for the data
 
-        # 建立圖片
+        # topFrame
+        self.topFrame = ttk.Labelframe(self)
+        self.topFrame.pack(side=tk.TOP)
+
+        # bottomFrame
+        self.bottomFrame = ttk.LabelFrame(self)
+        self.bottomFrame.pack()
+
+        # add pic
         pic001Image1 = Image.open("./images/pic001.jpg")
         self.pic001 = ImageTk.PhotoImage(pic001Image1)
-        self.canvas = tk.Canvas(self, width=500, height=250)  # 畫布
+        self.canvas = tk.Canvas(self.topFrame, width=500, height=250)
         self.canvas.pack()
         self.canvas.create_image(0, 5, image=self.pic001, anchor='nw')
-        # 建立logo
+        # add logo
         logoImage = Image.open('./Images/co2logo.png')
         resizeImage = logoImage.resize((89, 82), Image.LANCZOS)
         self.logoTkimage = ImageTk.PhotoImage(resizeImage)
         self.canvas.create_image(20, 30, image=self.logoTkimage, anchor='nw')
-        # 建立Button
-        btn = Button(self, text='石油', width=10,                    height=2, bd='10', command=draw_oil_co2)  # 石油
-        btn.place(x=100, y=120)
 
-        btn = Button(self, text='煤礦', width=10,
-                    height=2, bd='10', command=draw_oil_co2)  # 煤礦圖
-        btn.place(x=200, y=120)
+        # Create the combobox
+        self.combobox_frame = ttk.Frame(self)
+        self.combobox_frame.pack(pady=10)
+        self.combobox_label = ttk.Label(self.combobox_frame, text="Select Country:")
+        self.combobox_label.pack(side=tk.LEFT)
+        self.combobox = ttk.Combobox(self.combobox_frame, state="readonly")
+        self.combobox.pack(side=tk.LEFT)
 
-        btn = Button(self, text='天然氣', width=10,
-                    height=2, bd='10', command=draw_oil_co2)  # 天然氣圖表
-        btn.place(x=300, y=120)
+        self.selectdata = datasource.getInfo()
 
-        btn = Button(self, text='甲烷', width=10,
-                    height=2, bd='10', command=draw_oil_co2)  # 天然氣圖表
-        btn.place(x=400, y=120)
-        # 建立medianFrame
-        medianFrame = ttk.Labelframe(self, text="各國家")
-        # 建立combobox
-        comboBoxFrame = ttk.LabelFrame(self, text="各國家")
-        comboBoxFrame.pack(side=tk.LEFT, fill=tk.Y, padx=30)
+        country_list = self.selectdata['country'].unique().tolist()
+        country_list = sorted(country_list)
+        country_list.insert(0, '請選擇國家別---')
+        self.combobox_values = tuple(country_list)
+        self.combobox['values'] = self.combobox_values
+        self.combobox.current(0)  # Select the first country by default
 
-        csv_filename = 'a.csv'
-        with open(csv_filename) as f:
-            reader = csv.reader(f)
-            lst = list(tuple(line) for line in reader)
-            lst.insert(0, '請選擇一個國家')
-        comboBoxValues = (lst)
+        self.combobox.bind("<<ComboboxSelected>>", self.update_treeview)
 
-        comboBox = ttk.Combobox(comboBoxFrame, state="readonly")
-        comboBox.pack()
-        comboBox['values'] = comboBoxValues
-        comboBox.current(0)
-        comboBox.pack(side=tk.TOP)
+        # Create the treeview
+        columns = ('#1', '#2', '#3', '#4', '#5', '#6')
+        self.tree = ttk.Treeview(self, columns=columns, show='headings')
+        self.tree.heading('#1', text='year')
+        self.tree.column("#1", minwidth=0, width=200)
+        self.tree.heading('#2', text='co2')
+        self.tree.column("#2", minwidth=0, width=200)
+        self.tree.heading('#3', text='coal_co2')
+        self.tree.column("#3", minwidth=0, width=50)
+        self.tree.heading('#4', text='gas_co2')
+        self.tree.column("#4", minwidth=0, width=30)
+        self.tree.heading('#5', text='oil_co2')
+        self.tree.column("#5", minwidth=0, width=30)
+        self.tree.heading('#6', text='trade_co2')
+        self.tree.column("#6", minwidth=0, width=250)
 
-        medianFrame.pack(side=tk.LEFT)
-        # 建立bottomFrame
-        bottomFrame = ttk.Labelframe(self)
-        bottomFrame.pack(side=tk.LEFT)
-        # 建立treeViewFrame
-        treeViewFrame = ttk.Labelframe(self)
-        treeViewFrame.pack(side=tk.RIGHT)
-        # 建立treeview
-        columns = ('#1', '#2', '#3')
-        tree = ttk.Treeview(treeViewFrame, columns=columns, show='headings')
-        tree.heading('#1', text='country')
-        tree.column("#1", minwidth=0, width=75)
-        tree.heading('#2', text='population')
-        tree.column("#2", minwidth=0, width=100)
-        tree.heading('#3', text='co2')
-        tree.column("#3", minwidth=0, width=30)
-        tree.pack(side=tk.LEFT)
-        # ----------幫treeview加scrollbar----------------------------------
-        scrollbar = ttk.Scrollbar(treeViewFrame, command=tree.yview)
-        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-        tree.config(yscrollcommand=scrollbar.set)
-# 主程式
+        self.tree.pack(side=tk.LEFT)
+
+        for i in range(self.selectdata['country'].size):
+            self.tree.insert('', tk.END, values=[
+                 self.selectdata.iloc[i, 1], self.selectdata.iloc[i, 2], self.selectdata.iloc[i, 3],
+                 self.selectdata.iloc[i, 4], self.selectdata.iloc[i, 5], self.selectdata.iloc[i, 6]],
+                 tags=self.selectdata.index[i])
+
+    def update_treeview(self, event):
+        combobox = event.widget
+        selected_index = combobox.current()
+        selected_country = self.combobox_values[selected_index]
+        if selected_country != '請選擇國家別---':
+            filtered_data = self.selectdata[self.selectdata['country'] == selected_country]
+            self.tree.delete(*self.tree.get_children())
+            for i in range(filtered_data.shape[0]):
+                self.tree.insert('', tk.END, values=[
+                                 filtered_data.iloc[i, 1], filtered_data.iloc[i, 2], filtered_data.iloc[i, 3],
+                                 filtered_data.iloc[i, 4], filtered_data.iloc[i, 5], filtered_data.iloc[i, 6]],
+                                 tags=filtered_data.index[i])
+
+    def show_line_chart(self):
+        selected_country = self.combobox.get()
+        if selected_country != '請選擇國家別---':
+            filtered_data = self.selectdata[self.selectdata['country'] == selected_country]
+            years = filtered_data['year']
+            co2_values = filtered_data['co2']
+            plt.plot(years, co2_values)
+            plt.xlabel('Year')
+            plt.ylabel('CO2 Emissions')
+            plt.title(f'CO2 Emissions in {selected_country}')
+            plt.show()
+
+    def create_widgets(self):
+        # Create the plot button
+        self.plot_button = ttk.Button(self.bottomFrame, text="Show Line Chart", command=self.show_line_chart)
+        self.plot_button.pack()
+
+    def run(self):
+        self.title("Country Data")
+        self.geometry("1220x580")
+        self.create_widgets()
+        self.mainloop()
 
 
-def main():
-
-    try:
-        api_list = CarbonFootPrint.download_aqi()
-        #print(type(api_list))
-
-    except Exception as err:
-        #print(str(err))
-        return
-
-    window = Window()
-    window.title("碳足跡CarbonFootPrint")
-    window.mainloop()
-
-
-# 啟動點
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    window = MyWindow()
+    window.run()
